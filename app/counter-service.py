cb40fa2d11ab72504d9b8585f3a700c112aa48c7
Edgar Green <edgar.green@cybereason.com>
2024-07-28 16:53:34 +0100

    app stuff

--- app/counter-service.py
+++ app/counter-service.py
@@ -1,3 +1,22 @@
+#  #!flask/bin/python
+# from flask import Flask, request, request_started
+#
+# app = Flask(__name__)
+# post_counter = 0
+# get_counter = 0
+# @app.route('/', methods=["POST", "GET"])
+# def index():
+#     global post_counter, get_counter
+#     if request.method == "POST":
+#         post_counter+=1
+#         return "Hmm, Plus 1 to POST please "
+#     else:
+#         get_counter+=1
+#         return str(f"Our POST counter is: {post_counter} and GET counter is: {get_counter} ")
+#
+# if __name__ == '__main__':
+#     app.run(debug=True,port=8080,host='0.0.0.0')
+
  #!flask/bin/python
 from flask import Flask, request, request_started
 
@@ -6,15 +25,13 @@ post_counter = 0
 get_counter = 0
 @app.route('/', methods=["POST", "GET"])
 def index():
-    global post_counter, get_counter
+    global post_counter
+    global get_counter
     if request.method == "POST":
         post_counter+=1
-        return "Hmm, Plus 1 to POST please "
-    elif request.method == "GET":
+        return "Hmm, Plus 1 please..."
+    else:
         get_counter+=1
-        return "Hmm, Plus 1 to GET please "
-
-    return str(f"Our POST counter is: {post_counter} and GET counter is: {get_counter} ")
-
+        return str(f"Our POST counter is: {post_counter} and our GET counter is {get_counter} ")
 if __name__ == '__main__':
     app.run(debug=True,port=80,host='0.0.0.0')
